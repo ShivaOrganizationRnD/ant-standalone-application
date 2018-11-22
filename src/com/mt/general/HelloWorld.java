2f6f2e31ccccc674f5cad29d5a7aefe1398063bc
smayappa <m.shivamurthy@gmail.com>
2018-11-22 17:28:40 +0530

    ide commit

--- src/com/mt/general/HelloWorld.java
+++ src/com/mt/general/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to ANT World!! " + getLocalCurrentDate());
-
+		System.out.println("Hello Welcome to ANT World!! " + getLocalCurrentDate());
 		/*
 		 * String testCase;
 		 * 
